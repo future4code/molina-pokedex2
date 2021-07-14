78fc0801544d3f987459a2322bc51110d2c6fe06
Izabella <bellacbrandao@gmail.com>
2021-07-14 09:02:42 -0300

    :arrow_up: adicionando cores aos cards

--- molina-pokedex2/src/Hooks/GetPredominatColor.js
+++
@@ -1,8 +0,0 @@
-import { usePalette } from 'react-palette'
-
-const GetPredominatColor = (url) => {
-    const { data, loading, error } = usePalette(url)
-    return data.vibrant
-}
-
-export default GetPredominatColor
