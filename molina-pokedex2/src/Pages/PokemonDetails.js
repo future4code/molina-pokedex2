cbd5f1b40a885695520aec3e9e72fa3854fe5c08
Vitória Carolina Mochovik <mochovik.vc@gmail.com>
2021-07-17 16:17:42 -0300

    :art: Estilização básica da página de detalhes

--- molina-pokedex2/src/Pages/PokemonDetails.js
+++ molina-pokedex2/src/Pages/PokemonDetails.js
@@ -1,7 +1,9 @@
 import React, {useContext, useEffect, useState} from 'react'
 import { useParams } from 'react-router-dom'
 import { GlobalStateContext } from '../global/GlobalStateContext'
-import {ContaineCads, CardAtaques, CardPoderes, CardTipo,ContainerMain} from './styled'
+import {ContaineCads, CardAtaques, CardPoderes, CardTipo,
+    ContainerMain, ContainerDetails, ImgFront, ImgBack, 
+    ContainerImg, ContainerInformationOne, ContainerTipo} from './styled'
 import Header from '../components/Header/Header';
 
 const PokemonDetails = () => {
@@ -33,9 +35,9 @@ const PokemonDetails = () => {
         if(details?.types){
             return details.types.map((type) => {
                 return(
-                    <div>
+                    <CardTipo>
                     <p> {type.type.name} </p>
-                </div>
+                </CardTipo>
                 )
             })
         }
@@ -49,59 +51,93 @@ const PokemonDetails = () => {
               return details.moves.map((move) => {
                 return(
                     <div>
-                    <p> {move.move.name} </p>
-                </div>
+                        <p> {move.move.name} </p>
+                    </div>
                 )
             })  
             
         }
         return '<></>'
     }
+
+    const colorVerification = (caracteristica) => {
+       
+        if(caracteristica > 50){
+            return `228B22`
+        } else if( caracteristica < 50) {
+            return  `FF0000`
+        } else if( caracteristica === 50) {
+            return `FFD700`
+        }
+
+        
+    }
+    
   
     return(
         <div>
             <Header/>
-            <ContainerMain>
-            {details ? 
-                <ContaineCads style={{backgroundColor: `#${params.color}`}}>
-                    <img src= {details.sprites.front_default} />
-                    <img src= {details.sprites.back_default} />
-                </ContaineCads>
-            : <></>}
-            {details  ?
-                <div>
-                    <CardPoderes>
-                        <h2> Poderes </h2>
-                        <p> hp: {details.stats[0].base_stat} </p>
-                        <p> attack: {details.stats[1].base_stat} </p>
-                        <p> defense: {details.stats[2].base_stat} </p>
-                        <p> special-attack: {details.stats[3].base_stat} </p>
-                        <p> special-defense: {details.stats[4].base_stat} </p>
-                        <p> speed: {details.stats[5].base_stat} </p>
-                    </CardPoderes>
-                    
-                </div>
-            : <p> Carregando </p> }
-            
-            
-            <CardAtaques>
-                <CardTipo>
-                    <h4> Tipo </h4>
-                    {types()}
-                </CardTipo>
-                <h2> Principais ataques </h2>
-                {/* {moves()} */}
+            { details &&
+            <ContainerMain style={{backgroundColor: `#${params.color}`}}>
+                <ContainerTipo>
+                            {types()}
+                        </ContainerTipo>
                 {details ? 
-                    <div> 
-                        <p> { details.moves[0].move.name} </p> 
-                        <p>{ details.moves[1].move.name}  </p>
-                        <p>  { details.moves[2].move.name} </p>
-                        <p>{ details.moves[3].move.name}  </p> 
-                        <p> { details.moves[4].move.name} </p>
-                    </div>
-                : <p> calma </p>}
-            </CardAtaques>
-            </ContainerMain>
+                    <ContaineCads >
+                        <ContainerImg>
+                                <ImgFront src= {details.sprites.front_default} />
+                                <ImgBack src= {details.sprites.back_default} />
+                            </ContainerImg>
+                    </ContaineCads>
+                : <></>}
+                <ContainerDetails >
+                    {details  ?
+                        <ContainerInformationOne>
+                            <CardPoderes>
+                                <h2> Poderes </h2>
+                                <p> 
+                                    <b>HP: </b> {details.stats[0].base_stat} 
+                                    <b style={{color: `#${colorVerification(details.stats[0].base_stat)}`}}>___________________________________________________________</b>
+                                </p>
+                                <p> 
+                                    <b> Attack:</b> {details.stats[1].base_stat} 
+                                    <b style={{color: `#${colorVerification(details.stats[1].base_stat)}`}}>_______________________________________________________</b>
+                                </p>
+                                <p> 
+                                    <b>Defense: </b> {details.stats[2].base_stat} 
+                                    <b style={{color: `#${colorVerification(details.stats[2].base_stat)}`}}>______________________________________________________</b>
+                                </p>
+                                <p> 
+                                    <b>Special-Attack: </b> {details.stats[3].base_stat} 
+                                    <b style={{color: `#${colorVerification(details.stats[3].base_stat)}`}}>________________________________________________</b>
+                                </p>
+                                <p> 
+                                    <b> Special-Defense:</b> {details.stats[4].base_stat} 
+                                    <b style={{color: `#${colorVerification(details.stats[4].base_stat)}`}}>______________________________________________</b>
+                                </p>
+                                <p> 
+                                    <b>Speed: </b> {details.stats[5].base_stat} 
+                                    <b style={{color: `#${colorVerification(details.stats[5].base_stat)}`}}>_______________________________________________________</b>
+                                </p>
+                            </CardPoderes>
+                        </ContainerInformationOne>
+                    : <p> Carregando </p> }
+                    <CardAtaques>
+                        
+                        <h2> Principais ataques </h2>
+                        {/* {moves()} */}
+                        {details ? 
+                            <div> 
+                                <p> { details.moves[0].move.name} </p> 
+                                <p>{ details.moves[1].move.name}  </p>
+                                <p>  { details.moves[2].move.name} </p>
+                                <p>{ details.moves[3].move.name}  </p> 
+                                <p> { details.moves[4].move.name} </p>
+                            </div>
+                        : <p> calma </p>}
+                    </CardAtaques>
+                </ContainerDetails>
+            </ContainerMain> }
         </div>
     )
 }
--- molina-pokedex2/src/Pages/styled.js
+++ molina-pokedex2/src/Pages/styled.js
@@ -2,46 +2,85 @@ import styled from 'styled-components'
 
 export const ContaineCads = styled.div`
     display: flex;
-    flex-direction: column;
+    /* flex-direction: column; */
     align-items: center;
     justify-content: center;
-    width: 250px;
-    height: 250px;
+    /* width: 100vw;
+    height: 250px; */
     /* background-color: #0000CD; */
     padding: 8px;
-    border-radius: 16px;
-    margin-right: 55px;
+    /* border-radius: 16px;
+    margin-right: 55px; */
 
     h2{
         color: white;
     }
 
-    #containerImg{
-        /* background-color: white; */
-        padding: 8px;
-    }
+    
+
+
+`
+
+export const ContainerImg = styled.div`
+    margin-top: 180px;
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    width: 100%;
+    /* background-color: blue; */
+`
+
+export const ContainerInformationOne= styled.div`
+    /* background-color: purple; */
+
+`
+
+export const ImgBack = styled.img`
+    width: 200px;
+`
+
+export const ImgFront = styled.img`
+    width: 200px;
 
 
 `
 
 export const CardAtaques = styled.div`
-    /* background-color: red; */
+    display: flex;
+    flex-direction: column;
+    margin-left: 15px;
+    
     h2 {
         text-align: center;
     }
-   
+    
+   p{
+    margin-left: 270px;
+
+   }
 
 `
 
 export const CardPoderes = styled.div`
-    /* background-color: blue;  */
+    align-items: center;
     display: flex;
     flex-direction: column;
-    width: 250px;
+    margin-top: 45px;
+    
+    
 
 `
-export const CardTipo = styled.div`
-    /* background-color: yellow; */
+export const ContainerTipo = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+    width: 100%;
+    
+    margin-top: 30px;
+   
+
 
 
 `
@@ -49,6 +88,39 @@ export const ContainerMain = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    width: 100vw;
-    /* background-color: purple; */
+    flex-direction: column;
+    width: 100%;
+   
+    position: absolute;
+   
+`
+
+export const CardTipo = styled.div`
+    background-color: rgb(255, 255, 255, 0.5);
+    color: white;
+    border-radius: 15px;
+    /* width: 150%;
+    height: 100%; */
+
+    width: 5%;
+    padding: 1px;
+    margin: 5px;
+    text-align: center;
+`
+
+export const ContainerDetails = styled.div`
+    background-color: rgb(255, 255, 255, 0.7);
+    align-items: center;
+    justify-content: center;
+    padding: 35px;
+    border-radius: 55px;
+    /* position: absolute; */
+   
+    margin-bottom: 80px;
+    margin-top: 100px;
+    width: 80%;
+    margin-left: 15px;
+    margin-right: 15px;
+    
+
 `
